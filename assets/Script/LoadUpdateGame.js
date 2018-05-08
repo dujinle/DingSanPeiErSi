52a2f65259ab844a1b17ed31c042c84a49936bd3
dujinle <yingyongdujinle@163.com>
2018-05-08 22:32:06 +0800

    update for hot update

---
+++ assets/Script/LoadUpdateGame.js
@@ -0,0 +1,41 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        loadBar:cc.ProgressBar,
+        precent:cc.Label,
+		rate:0,
+		source_leng:0,
+    },
+    onLoad () {
+		this.source_leng = 50;
+		this.load_res();
+        this.schedule(this.load_update,0.5);
+    },
+	load_update(){
+		this.loadBar.progress = this.rate/this.source_leng * 100;
+		this.precent.string = this.loadBar.progress + "%";
+		cc.log("this.rate:" + this.rate);
+		if(this.rate >= this.source_leng){
+			this.unschedule(this.load_update);
+			cc.director.loadScene("LoginScene");
+		}
+	},
+	load_res(){
+		var self = this;
+		cc.loader.loadResDir("",cc.SpriteFrame,function (err, assets) {
+			for(var i = 0;i < assets.length;i++){
+				g_assets[assets[i].name] = assets[i];
+				self.rate = self.rate + 1;
+				cc.log("load res :" + assets[i].name);
+			}
+		});
+		cc.loader.loadResDir("prefab",function (err, assets) {
+			for(var i = 0;i < assets.length;i++){
+				g_assets[assets[i].name] = assets[i];
+				self.rate = self.rate + 1;
+				cc.log("load res :" + assets[i].name);
+			}
+		});
+	}
+});
