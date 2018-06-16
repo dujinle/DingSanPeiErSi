a35a0c06ce437b0a82af711ef31454b50a252d96
dujinle <yingyongdujinle@163.com>
2018-06-17 00:15:52 +0800

    添加公告界面

---
+++ assets/Script/common/pop_gonggao.js
@@ -0,0 +1,47 @@
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        bg_sprite:cc.Node,
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+    onLoad() {
+        cc.log("start go into pop game gonggao js");
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            swallowTouches: true,
+            // 设置是否吞没事件，在 onTouchBegan 方法返回 true 时吞没
+            onTouchBegan: function (touch, event) {
+                return true;
+            },
+            onTouchMoved: function (touch, event) {            // 触摸移动时触发
+            },
+            onTouchEnded: function (touch, event) {            // 点击事件结束处理
+                var target=event.getCurrentTarget();
+                var local=target.convertToNodeSpace(touch.getLocation());
+                var s = target.getContentSize();
+                var rect = cc.rect(0, 0, s.width, s.height);
+                if (cc.rectContainsPoint(rect, local)){
+                    cc.log("ok touch in the region......");
+                }else{
+                    cc.log("touch remove from parent");
+                    self.node.active = false;
+                }
+            }
+         }, self.bg_sprite);
+    },
+
+    // update (dt) {},
+});
